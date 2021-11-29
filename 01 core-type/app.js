40cd199e74d743be0d43b36318960d2acff445fe
静默 <cau1403090523@gmail.com>
2021-11-30 06:17:34 +0800

    update

--- 01 core-type/app.js
+++ 01 core-type/app.js
@@ -1,6 +1,12 @@
-function add(a, b) {
-    return a + b;
+function add(a, b, printResult, resultPhrase) {
+    var result = a + b;
+    if (showResult) {
+        console.log(resultPhrase + result);
+    }
+    return result;
 }
 var a = 5;
 var b = 2.8;
-console.log(add(a, b));
+var resultPhrase = "Result is ";
+var showResult = true;
+add(a, b, showResult, resultPhrase);
--- 01 core-type/app.ts
+++ 01 core-type/app.ts
@@ -1,6 +1,12 @@
-function add(a: number, b: number) {
-  return a + b;
+function add(a: number, b: number, printResult: boolean, resultPhrase: string) {
+  const result = a + b;
+  if (showResult) {
+    console.log(resultPhrase + result);
+  }
+  return result;
 }
 const a = 5;
 const b = 2.8;
-console.log(add(a, b));
+const resultPhrase = "Result is ";
+const showResult = true;
+add(a, b, showResult, resultPhrase);
