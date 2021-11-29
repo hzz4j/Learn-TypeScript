dfc0218dd10bb53bb1b93cda49c11ed90d7cbc51
静默 <cau1403090523@gmail.com>
2021-11-30 06:03:26 +0800

    update

---
+++ 01 core-type/app.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+var a = 5;
+var b = 2.8;
+console.log(add(a, b));
---
+++ 01 core-type/app.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+const a = 5;
+const b = 2.8;
+console.log(add(a, b));
