fd54a5a06c53ab2b77e06d7594b1278be8331a59
静默 <cau1403090523@gmail.com>
2021-11-30 17:43:40 +0800

    private constructor

--- 04 class/app.js
+++ 04 class/app.js
@@ -7,9 +7,16 @@ class Department {
 }
 class ITDepartment extends Department {
     constructor(id) {
-        super(id, 'IT');
+        super(id, "IT");
     }
     describe() {
-        console.log('IT Department id - ' + this.id);
+        console.log("IT Department id - " + this.id);
+    }
+    static getInstance() {
+        if (ITDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new ITDepartment("idx");
+        return this.instance;
     }
 }
--- 04 class/app.ts
+++ 04 class/app.ts
@@ -3,13 +3,21 @@ abstract class Department {
   abstract describe(this: Department): void;
 }
 
-class ITDepartment extends Department{
+class ITDepartment extends Department {
+  static instance: ITDepartment;
+  private constructor(id: string) {
+    super(id, "IT");
+  }
 
-    constructor(id:string){
-        super(id,'IT');
-    }
+  describe(this: ITDepartment) {
+    console.log("IT Department id - " + this.id);
+  }
 
-    describe(this: ITDepartment) {
-        console.log('IT Department id - '+this.id);
+  static getInstance() {
+    if (ITDepartment.instance) {
+      return this.instance;
     }
+    this.instance = new ITDepartment("idx");
+    return this.instance;
+  }
 }
