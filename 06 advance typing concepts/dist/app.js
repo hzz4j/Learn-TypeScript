29edff7e57b0c8d74326e88c37be89b42ea2cd93
静默 <cau1403090523@gmail.com>
2021-12-01 06:56:20 +0800

    type casting

--- 06 advance typing concepts/dist/app.js
+++ 06 advance typing concepts/dist/app.js
@@ -1,53 +1,6 @@
 "use strict";
-function printEmployeeInfomation(emp) {
-    console.log("Name: " + emp.name);
-    if ("privileges" in emp) {
-        // type guard
-        console.log("Privileges: " + emp.privileges);
-    }
-    if ("startDate" in emp) {
-        console.log("startDate: " + emp.startDate);
-    }
-}
-printEmployeeInfomation({
-    name: "静默",
-    privileges: ["create-server"],
-    startDate: new Date(),
-});
-class Car {
-    drive() {
-        console.log("Driving ... ...");
-    }
-}
-class Truck {
-    // 卡车
-    drive() {
-        console.log("Driving a truck... ...");
-    }
-    // cargo 货物
-    loadCargo(amount) {
-        console.log(`Loading ${amount} cargo... ...`);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    // if ("loadCargo" in vehicle)
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(10);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case "Bird":
-            speed = animal.flyingSpeed;
-            break;
-        case "Horse":
-            speed = animal.runningSpeed;
-    }
-    console.log("Moving at speed: " + speed);
+const userInputElement = document.getElementById("user-input");
+// 不用！自己来检测
+if (userInputElement) {
+    userInputElement.value = "Hello TypeScript";
 }
