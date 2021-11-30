1e9f378d311ba7af7f0bf828377efafc0f86b82c
静默 <cau1403090523@gmail.com>
2021-11-30 12:06:18 +0800

    代码调试

---
+++ 02 compiler/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+const button = document.querySelector("button");
+console.log(button);
+button === null || button === void 0 ? void 0 : button.addEventListener('click', (e) => {
+    console.log("Clicked!");
+});
+//# sourceMappingURL=app.js.map
