822260e1c74a3c73c88952807dcbd26224336b0b
静默 <cau1403090523@gmail.com>
2021-11-30 18:03:01 +0800

    interface

---
+++ 05 interfaces/dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let user1 = {
+    name: "静默",
+    score: 100,
+    greet(phrase) {
+        console.log("Greet: " + phrase);
+    },
+};
--- 05 interfaces/src/app.ts
+++ 05 interfaces/src/app.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+interface Person {
+  name: string;
+  score: number;
+  greet(phrase: string): void;
+}
+
+let user1: Person = {
+  name: "静默",
+  score: 100,
+  greet(phrase: string) {
+    console.log("Greet: " + phrase);
+  },
+};
