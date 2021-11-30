83e1a97711ef8580da2fb061338ec90edb2da3b8
静默 <cau1403090523@gmail.com>
2021-11-30 17:10:28 +0800

    getter and setter

--- 04 class/app.js
+++ 04 class/app.js
@@ -1,40 +1,38 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Department = /** @class */ (function () {
-    function Department(id, name) {
+"use strict";
+class Department {
+    constructor(id, name) {
         this.id = id;
         this.name = name;
     }
-    return Department;
-}());
-var AccountingDepartment = /** @class */ (function (_super) {
-    __extends(AccountingDepartment, _super);
-    function AccountingDepartment(id, reports) {
-        var _this = _super.call(this, id, "Accounting") || this;
-        _this.reports = reports;
-        return _this;
+}
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, "Accounting");
+        this.reports = reports;
+        this.lastReport = reports[0];
     }
-    AccountingDepartment.prototype.addReport = function (report) {
+    // getter
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("No report found.");
+    }
+    // setter
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error("Please pass in a valid value!");
+        }
+        this.addReport(value);
+    }
+    addReport(report) {
         this.reports.push(report);
-    };
-    AccountingDepartment.prototype.printReports = function () {
+        this.lastReport = report;
+    }
+    printReports() {
         console.log(this.reports);
-    };
-    return AccountingDepartment;
-}(Department));
-var accountingDepartment = new AccountingDepartment('idx', []);
-accountingDepartment.addReport('Something error happen.');
-accountingDepartment.printReports();
+    }
+}
+const accountingDepartment = new AccountingDepartment("idx", []);
+accountingDepartment.mostRecentReport = 'Hello Report';
+console.log(accountingDepartment.mostRecentReport);
--- 04 class/app.ts
+++ 04 class/app.ts
@@ -3,12 +3,30 @@ class Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+  // getter
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+  // setter
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
   }
 
   addReport(report: string) {
     this.reports.push(report);
+    this.lastReport = report;
   }
 
   printReports() {
@@ -16,6 +34,7 @@ class AccountingDepartment extends Department {
   }
 }
 
-const accountingDepartment = new AccountingDepartment('idx',[]);
-accountingDepartment.addReport('Something error happen.');
-accountingDepartment.printReports();
+const accountingDepartment = new AccountingDepartment("idx", []);
+accountingDepartment.mostRecentReport = 'Hello Report';
+console.log(accountingDepartment.mostRecentReport);
+
