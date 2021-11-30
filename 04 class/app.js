cd5fbe37386d0561e473f80cb58e27e5808421f3
静默 <cau1403090523@gmail.com>
2021-11-30 15:30:56 +0800

    read only

--- 04 class/app.js
+++ 04 class/app.js
@@ -1,7 +1,12 @@
 var Department = /** @class */ (function () {
     function Department(id, n) {
+        this.id = id;
+        this.n = n;
         this.employees = [];
-        this.name = n;
     }
+    Department.prototype.updateId = function () {
+        // 不能更新
+        this.id = 'xxx';
+    };
     return Department;
 }());
--- 04 class/app.ts
+++ 04 class/app.ts
@@ -1,5 +1,10 @@
 class Department {
   private employees: string[] = [];
 
-  constructor(private id: string, private n: string) {}
+  constructor(private readonly id: string, private n: string) {}
+
+  updateId(){
+      // 不能更新 error
+      //this.id = 'xxx';
+  }
 }
