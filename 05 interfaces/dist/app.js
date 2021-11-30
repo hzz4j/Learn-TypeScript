1d49adc96fbd04643de1e35f5ea800b99869f20b
静默 <cau1403090523@gmail.com>
2021-11-30 18:17:59 +0800

    class 实现接口

--- 05 interfaces/dist/app.js
+++ 05 interfaces/dist/app.js
@@ -1,8 +1,12 @@
 "use strict";
-let user1 = {
-    name: "静默",
-    score: 100,
+class Person {
+    constructor(n) {
+        this.score = 100;
+        this.name = n;
+    }
     greet(phrase) {
-        console.log("Greet: " + phrase);
-    },
-};
+        console.log(phrase + this.name);
+    }
+}
+let user1 = new Person("静默");
+user1.greet('Hi there,I\'m ');
--- 05 interfaces/src/app.ts
+++ 05 interfaces/src/app.ts
@@ -1,13 +1,22 @@
-interface Person {
+interface Greetable {
   name: string;
-  score: number;
+
   greet(phrase: string): void;
 }
 
-let user1: Person = {
-  name: "静默",
-  score: 100,
-  greet(phrase: string) {
-    console.log("Greet: " + phrase);
-  },
-};
+class Person implements Greetable {
+  name: string;
+  score: number = 100;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase:string){
+      console.log(phrase+this.name);
+      
+  }
+}
+
+let user1: Greetable = new Person("静默");
+user1.greet('Hi there,I\'m ');
