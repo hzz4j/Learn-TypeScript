237815fba27926c1a44d126ce309d835604718d9
静默 <cau1403090523@gmail.com>
2021-12-01 15:49:17 +0800

    decorators

---
+++ 08 decorators/dist/app.js
@@ -0,0 +1,32 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logging(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
+}
+function withTemplate(template, hookId) {
+    return function (constructor) {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector("h1").textContent = p.name;
+        }
+    };
+}
+// decorator会在类定义的地方执行
+let Person = class Person {
+    constructor(name = "静默") {
+        this.name = name;
+    }
+};
+Person = __decorate([
+    withTemplate("<h1>Hello TypeScript</h1>", "app")
+], Person);
---
+++ 08 decorators/src/app.ts
@@ -0,0 +1,23 @@
+function Logging(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+function withTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// decorator会在类定义的地方执行
+@withTemplate("<h1>Hello TypeScript</h1>", "app")
+class Person {
+  constructor(public name: string = "静默") {}
+}
